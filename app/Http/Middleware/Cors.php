766636f340818a28a6533c0457e4ebc9a9e9a505
Kaspars Bulins <kasparsb@hotmail.com>
2022-09-05 23:02:11 +0300

    cors url ar ending slash

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Headers', 'x-requested-with')
-            ->header('Access-Control-Allow-Origin', 'https://www.la.lv')
+            ->header('Access-Control-Allow-Origin', 'https://www.la.lv/')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
     }
 }
